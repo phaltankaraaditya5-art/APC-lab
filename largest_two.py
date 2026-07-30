72d5195d1c2648c5d945a6254f8615c95a871492
phaltankaraaditya5-art <phaltankaraaditya5@gmail.com>
2026-07-30 09:37:07 +0530

    Add files via upload

---
+++ largest_two.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    print(a, "is the largest")
+else:
+    print(b, "is the largest")
---
+++ vowel.py
@@ -0,0 +1,6 @@
+ch = input("Enter a character: ")
+ch = ch.lower()
+if ch in ('a', 'e', 'i', 'o', 'u'):
+    print("The character is a vowel")
+else:
+    print("The character is a consonant")
