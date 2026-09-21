36dfcb449dc31d22a40650880d3e4ee13a352dd3
phaltankaraaditya5-art <phaltankaraaditya5@gmail.com>
2026-09-21 12:25:25 +0530

    Delete file_15_source.py

--- file_15_source.py
+++
@@ -1,8 +0,0 @@
-x = 5  # this is x
-y = 10  # this is y
-total = x + y  # adding x and y
-print(total)  # printing the result
-
-# this is a full comment line
-name = "Rehan"  # storing name
-print(name)
